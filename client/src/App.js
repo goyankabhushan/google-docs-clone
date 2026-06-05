754e9f9323faa81a6851c111d52154d1d25c618f
Bhushan Goyanka <goyankabhushan@gmail.com>
2026-06-05 23:26:21 +0530

    logo refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Editor from "./components/Editor";
+import logo from './assets/logo.png';
 
 import {
   BrowserRouter as Router,
@@ -18,6 +19,11 @@ function App() {
         <Route path="/docs/:id" element={<Editor />} />
       </Routes>
     </Router>
+  );
+}
+function Navbar() {
+  return (
+    <img src={logo} alt="Logo" width="50" />
   );
 }
 
